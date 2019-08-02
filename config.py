27ea071fabe39fafeb0335b44654f171fa1ebb7d
dlockmal <dlockmal@pop-os.localdomain>
2019-08-02 18:46:45 -0400

    another test

--- config.py
+++ config.py
@@ -28,8 +28,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-                              'sqlite:///' + 'mnt/flashcard_db/data-dev.sqlite'
-
+                              'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 class TestingConfig(Config):
     TESTING = True
